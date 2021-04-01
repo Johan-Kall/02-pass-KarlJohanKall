31eb11d00050ea025d8351d59b47eb8dbd51aa68
Johan-Kall <78425225+Johan-Kall@users.noreply.github.com>
2021-04-01 15:49:40 +0200

    Add files via upload

---
+++ src/test/java/com/testprogramming/maven/Assignment02/AppTest.java
@@ -0,0 +1,13 @@
+package com.testprogramming.maven.Assignment02;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}

d1ee7e950c67001bb608f2d38df64d8a8d2a19aa
Johan-Kall <78425225+Johan-Kall@users.noreply.github.com>
2021-04-02 14:46:45 +0200

    Final Upload 1

--- src/test/java/com/testprogramming/maven/Assignment02/CalculatorTest.java
+++ src/test/java/com/testprogramming/maven/Assignment02/CalculatorTest.java
@@ -3,11 +3,13 @@ package com.testprogramming.maven.Assignment02;
 //import java.lang.*;
 
 import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
 	BasicCalculator basic = new BasicCalculator();
 	AdvancedCalculator advanced = new AdvancedCalculator();
 	
+	@Test
 	void testAddition() {
 		assertEquals(basic.addition(2.4, 3.72),6.12, 0);
 		assertEquals(basic.addition(0, 0),0,0);
@@ -18,28 +20,31 @@ class CalculatorTest {
 		assertEquals(basic.addition(test1, test2), test1+test2,0);
 	}
 
+	@Test
 	void testSubtraction() {
 		assertEquals(basic.subtraction(3.72, 2.60),1.12, 0);
 		assertEquals(basic.subtraction(0, 0),0,0);
 		assertEquals(basic.subtraction(2.32, 2.32),0,0);
-		assertEquals(basic.subtraction(3.5, 6.13),-3.37,0);
+		assertEquals(basic.subtraction(3.5, 6.13),-2.63,0);
 		assertEquals(basic.subtraction(-3.5, -6.13),2.63,0);
 		double test1 = Math.random()*100;
 		double test2 = Math.random()*100;		
 		assertEquals(basic.subtraction(test1, test2), test1-test2,0);
 	}
 	
+	@Test
 	void testMultiplication() {
 		assertEquals(basic.multiplication(3, 2),6, 0);
 		assertEquals(basic.multiplication(1, 0),0,0);
 		assertEquals(basic.multiplication(2.32, 2),4.64,0);
 		assertEquals(basic.multiplication(-3, -4),12,0);
-		assertEquals(basic.multiplication(-3, 4),-12.63,0);
+		assertEquals(basic.multiplication(-3, 4),-12,0);
 		double test1 = Math.random()*100;
 		double test2 = Math.random()*100;		
 		assertEquals(basic.multiplication(test1, test2), test1*test2,0);
 	}
 	
+	@Test
 	void testDivision() {
 		assertEquals(basic.division(3, 2),1.5, 0);
 		assertEquals(basic.division(1, 0.5),2,0);
@@ -51,14 +56,16 @@ class CalculatorTest {
 		assertEquals(basic.division(test1, test2), test1/test2,0);
 	}
 	
+	@Test
 	void testSquare() {
 		assertEquals(advanced.square(2.5),6.25, 0);
 		assertEquals(advanced.square(0),0,0);
 		assertEquals(advanced.square(-3),9,0);
 		double test = Math.random()*100;
-		assertEquals(advanced.square(test), test,0);
+		assertEquals(advanced.square(test), test*test,0);
 	}
 
+	@Test
 	void testPower() {
 		assertEquals(advanced.power(-2, 3),-8, 0);
 		assertEquals(advanced.power(-1, 0),1,0);
@@ -68,7 +75,8 @@ class CalculatorTest {
 		double test2 = Math.random()*10;
 		assertEquals(advanced.power(test1, test2), Math.pow(test1, test2),0);
 	}
-	
+
+	@Test
 	void testSquareRoot() {
 		assertEquals(advanced.squareRoot(4),2, 0);
 		assertEquals(advanced.squareRoot(0),0,0);
@@ -77,7 +85,8 @@ class CalculatorTest {
 		double test = Math.random()*10;
 		assertEquals(advanced.squareRoot(test), Math.sqrt(test),0);
 	}
-	
+
+	@Test
 	void testAbsolute() {
 		assertEquals(advanced.absolute(-3),3, 0);
 		assertEquals(advanced.absolute(5.432),5.432,0);
@@ -85,7 +94,8 @@ class CalculatorTest {
 		double test = -Math.random()*10;
 		assertEquals(advanced.absolute(test), Math.abs(test),0);
 	}
-	
+
+	@Test
 	void testFactorial() {
 		assertEquals(advanced.factorial(4),24, 0);
 		assertEquals(advanced.factorial(5.432),120,0);
